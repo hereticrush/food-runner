0641c1470080d7d4942e7373dba35ebe04a484ed
hereticrush <auroraborealis853@gmail.com>
2022-07-13 17:28:41 +0300

    Add simple ui components into scrollview layout, such as edittext, button

---
+++ app/src/main/java/com/example/food_notes/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.food_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View fabAddContent = findViewById(R.id.fabAddContent);
+        fabAddContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNewUserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
